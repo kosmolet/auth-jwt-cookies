43c3797d4657083ce18fced6d1c523808dde4ce8
kosmolet <svet1199@gmail.com>
2021-01-13 14:02:53 +0100

    update user model

--- models/User.js
+++ models/User.js
@@ -1,13 +1,54 @@
+const crypto = require("crypto");
 const mongoose = require("mongoose");
 const { Schema } = mongoose;
+const bcrypt = require("bcryptjs");
+const jwt = require("jsonwebtoken");
+const SALT_WORK_FACTOR = 10;
 
 const userSchema = new Schema(
   {
-    username: { type: String, required: true },
-    email: { type: String, required: true },
-    password: { type: String, required: true },
+    username: {
+      type: String,
+      trim: true,
+      required: [true, "Please provide username"],
+      minlength: 3,
+    },
+    email: {
+      type: String,
+      unique: true,
+      lowercase: true,
+      required: [true, "Please provide email"],
+      trim: true,
+      match: [
+        /^(([^<>()[\]\\.,;:\s@"]+(\.[^<>()[\]\\.,;:\s@"]+)*)|(".+"))@((\[[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\.[0-9]{1,3}\])|(([a-zA-Z\-0-9]+\.)+[a-zA-Z]{2,}))$/,
+        "Please provide a valid email",
+      ],
+    },
+    password: {
+      type: String,
+      required: [true, "Please provide password"],
+      minlength: 6,
+      select: false,
+    },
+    resetPasswordToken: String,
+    resetPasswordExpire: Date,
   },
+
   { timestamps: true }
 );
 
+userSchema.pre("save", async function (next) {
+  if (!this.isModified("password")) return next();
+  try {
+    const salt = await bcrypt.genSalt(SALT_WORK_FACTOR);
+    this.password = await bcrypt.hash(this.password, salt);
+    return next();
+  } catch (err) {
+    return next(err);
+  }
+});
+
+schema.methods.validatePassword = async function validatePassword(data) {
+  return await bcrypt.compare(password, this.password);
+};
 module.exports = mongoose.model("User", userSchema);
